7a0200c94e27ae429545ecd02ab64eba54f4fc9e
shamank <russyakrusya@gmail.com>
2022-05-04 20:59:35 +0300

    init

--- app/apps.py
+++ app/apps.py
@@ -5,4 +5,4 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-    verbose_name = 'Приложение'
+    verbose_name = 'Приложениее'
